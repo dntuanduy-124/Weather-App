cfb97f4056919c59e2814f257f57804f13f726c2
Đỗ Ngọc Tuấn Duy <143875347+dntuanduy-124@users.noreply.github.com>
2024-10-20 11:16:40 +0700

    Update server.py

--- server.py
+++ server.py
@@ -16,7 +16,7 @@ def get_weather():
         city="Ho Chi Minh City"
         
     weather_data=get_current_weather(city)
-     #City not found by API
+     
     if not weather_data['cod'] == 200:
         return render_template('city-not-found.html')
         
